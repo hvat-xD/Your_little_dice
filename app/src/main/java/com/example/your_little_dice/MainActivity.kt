0a767ecbdf66f781eb3d7516b3d7cdf5dbd41efb
hvat-xD <kiriukhin.kiril@gmail.com>
2022-03-18 10:32:33 +0200

    added a dice images

--- app/src/main/java/com/example/your_little_dice/MainActivity.kt
+++ app/src/main/java/com/example/your_little_dice/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.your_little_dice
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.your_little_dice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
+        setContentView(binding.root)
+        //val rollButton: Button = findViewById(R.id.button)
+        binding.button.setOnClickListener {
             val currDice = Dice(6)
             rollDice(currDice)
         }
@@ -19,9 +21,16 @@ class MainActivity : AppCompatActivity() {
 
     fun rollDice(dice: Dice) {
         Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
-        val resultTextView: TextView = findViewById(R.id.textView)
         val currDice = Dice(6)
-        resultTextView.text = dice.roll().toString()
+        val currRoll = currDice.roll()
+        when(currRoll) {
+            1->binding.imageView.setImageResource(R.drawable.dice_1)
+            2->binding.imageView.setImageResource(R.drawable.dice_2)
+            3->binding.imageView.setImageResource(R.drawable.dice_3)
+            4->binding.imageView.setImageResource(R.drawable.dice_4)
+            5->binding.imageView.setImageResource(R.drawable.dice_5)
+            6->binding.imageView.setImageResource(R.drawable.dice_6)
+        }
     }
 }
 
