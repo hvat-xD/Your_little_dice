a14694320ac00ebd207c52884422707d0033658c
hvat-xD <kiriukhin.kiril@gmail.com>
2022-03-19 15:54:11 +0200

    fix toast on start of program

--- app/src/main/java/com/example/your_little_dice/MainActivity.kt
+++ app/src/main/java/com/example/your_little_dice/MainActivity.kt
@@ -6,34 +6,43 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.your_little_dice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    public var flagForToast = false
+    //binding for convenient coding
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        //val rollButton: Button = findViewById(R.id.button)
-        binding.button.setOnClickListener {
+        //button on click listener
+        binding.butRoll.setOnClickListener {
             val currDice = Dice(6)
             rollDice(currDice)
         }
+        var startDice = Dice(6)
+        rollDice(startDice)
+        flagForToast = true
     }
-
-    fun rollDice(dice: Dice) {
-        Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+    //method for rolling a dice in activity
+    private fun rollDice(dice: Dice) {
+        //show toast message
+        if (flagForToast)Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+        //create and roll a dice
         val currDice = Dice(6)
         val currRoll = currDice.roll()
-        when(currRoll) {
-            1->binding.imageView.setImageResource(R.drawable.dice_1)
-            2->binding.imageView.setImageResource(R.drawable.dice_2)
-            3->binding.imageView.setImageResource(R.drawable.dice_3)
-            4->binding.imageView.setImageResource(R.drawable.dice_4)
-            5->binding.imageView.setImageResource(R.drawable.dice_5)
-            6->binding.imageView.setImageResource(R.drawable.dice_6)
-        }
+        //change contDescription and imgResource
+        binding.ivDice.contentDescription = currRoll.toString()
+        binding.ivDice.setImageResource(when(currRoll) {
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else->R.drawable.dice_6
+        })
     }
 }
-
+//dice class with roll function
 class Dice(var sides: Int) {
     fun roll(): Int {
         return (1..sides).random()
